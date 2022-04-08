83ec365b0ec35fb6e74cef703584d76857e543ca
Pritam Debnath <pritamdebnath297@gmail.com>
2022-04-08 12:47:43 +0530

    package running, iOS working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Todo());
@@ -9,7 +10,17 @@ class Todo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(title: Text('To-do'),),)
+      theme: ThemeData(
+        textTheme: GoogleFonts.nunitoSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: Scaffold(
+        body: Container(
+          child: Center(child:Text("hello world!!!!", style: TextStyle(fontSize: 50.0),),),
+        ),
+      )
     );
   }
 }
+
