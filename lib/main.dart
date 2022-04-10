6e0428f253ff9b893d3a3d71f6ab0af3a94da2a2
Pritam Debnath <pritamdebnath297@gmail.com>
2022-04-11 00:00:03 +0530

    understood private properties of a class with _

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(Todo());
+  runApp(MyFirstApp());
 }
 
-class Todo extends StatelessWidget {
+class MyFirstApp extends StatefulWidget {
+  @override
+  State<MyFirstApp> createState() => _MyFirstAppState();
+}
+
+class _MyFirstAppState extends State<MyFirstApp> {
+  void _answerQuestion() {
+    setState(() {
+      _questionINdex = _questionINdex + 1;
+    });
+    print("Pressed Button!");
+  }
+
+  var _questionINdex = 0;
 
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What\'s your favorite hero?',
+      'What\'s your favourite anime?',
+      'Question 3',
+      'Question 4',
+    ];
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: GoogleFonts.nunitoSansTextTheme(
-          Theme.of(context).textTheme,
-        ),
-      ),
       home: Scaffold(
-        body: Container(
-          child: Center(child:Text("hello world!!!!", style: TextStyle(fontSize: 50.0),),),
+        appBar: AppBar(
+          title: Text("List"),
         ),
-      )
-    );
+        body: Column(children: [
+          Text(questions[_questionINdex]),
+          ElevatedButton(
+            onPressed: _answerQuestion,
+            child: Text('Answer1'),
+          ),
+          ElevatedButton(
+            onPressed: _answerQuestion,
+            child: Text('Answer2'),
+          ),
+        ]),
+      ),
+    ); //this is a widget which is a combination of all the widgets you are gonna make, and this is gonna be returned by build method
   }
 }
 
+// class Todo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         theme: ThemeData(
+//           textTheme: GoogleFonts.nunitoSansTextTheme(
+//             Theme.of(context).textTheme,
+//           ),
+//         ),
+//         home: Scaffold(
+//           appBar: AppBar(
+//             title: Text("Counting App"),
+//           ),
+//           body: Container(
+//             child: Center(
+//               child: Text(
+//                 "hello world!!!!",
+//                 style: TextStyle(fontSize: 50.0),
+//               ),
+//             ),
+//           ),
+//         ));
+//   }
+// }
